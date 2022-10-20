f86553609a408ca8c57846ffa9a2fdcae1df8ac3
Surachat03 <gametrinity2016@hotmail.com>
2022-10-21 06:56:45 +0700

    Exercise4

---
+++ Exercise4_Surachat_Loednaweporn.py
@@ -0,0 +1,14 @@
+F = "Foundation English"
+G = "General Business"
+I = "Introduction to Computer Systems"
+C = "Computer Programming"
+F1 = "81"
+G1 = 75
+I1 = 86
+C1 = 73
+
+print("Your Score")
+print(F,":",F1)
+print(G,":",G1)
+print(I,":",I1)
+print(C,":",C1)
