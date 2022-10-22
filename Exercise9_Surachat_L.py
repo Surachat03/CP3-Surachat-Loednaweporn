706d1e718894cef45301162a5c440080d65e84ac
Surachat03 <gametrinity2016@hotmail.com>
2022-10-23 01:19:34 +0700

    Exercise9

---
+++ Exercise9_Surachat_L.py
@@ -0,0 +1,8 @@
+usernameInput = input("Username :")
+passwordInput = input("Password :")
+while usernameInput != "Hajime" or passwordInput !=("Fury"):
+    print("Username or Password is incorrect")
+    print("Please Try Again")
+    usernameInput = input("Username :")
+    passwordInput = input("Password :")
+print("Welcome !")
