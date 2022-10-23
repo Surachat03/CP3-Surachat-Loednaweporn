623ae4228b44584c034bdc5284820f04ff6ef58b
Surachat03 <gametrinity2016@hotmail.com>
2022-10-24 06:04:27 +0700

    Lecture50

---
+++ Lecture50_Surachat_L.py
@@ -0,0 +1,8 @@
+def addNumber(x,y):
+    print("ผล + ของNumber",x+y)
+    print("ผล - ของNumber",x-y)
+    print("ผล * ของNumber",x*y)
+    print("ผล / ของNumber",x/y)
+x = int(input("Number 1 :"))
+y = int(input("Number 2 :"))
+addNumber(x,y)
